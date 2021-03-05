11447add72bcb602c3213c1093ec89037806af30
yiiliveext <yiiliveext@gmail.com>
2021-03-05 10:24:24 +0200

    Fix ShellCommand, romove ServiceLocator and functions

--- src/ShellCommand.php
+++ src/ShellCommand.php
@@ -7,8 +7,10 @@ use Psy\Configuration;
 use Psy\Shell;
 use Psy\VersionUpdater\Checker;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\CommandLoader\CommandLoaderInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Yiisoft\Yii\Console\CommandLoader;
 
 class ShellCommand extends Command
 {
@@ -16,10 +18,13 @@ class ShellCommand extends Command
 
     private ContainerInterface $container;
 
-    public function __construct(ContainerInterface $container)
+    private CommandLoaderInterface $commandLoader;
+
+    public function __construct(ContainerInterface $container, CommandLoaderInterface $commandLoader)
     {
         parent::__construct();
         $this->container = $container;
+        $this->commandLoader = $commandLoader;
     }
 
     protected function configure(): void
@@ -31,21 +36,24 @@ class ShellCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        ServiceLocator::getInstance()->registerContainer($this->container);
-
         $config = Configuration::fromInput($input);
         $config->setUpdateCheck(Checker::NEVER);
         $config->setStartupMessage('Let\'s make Yii great again!');
         $shell = new Shell($config);
-        $shell->setIncludes($this->getIncludes());
+        $shell->addCommands($this->getCommands());
+        $shell->setScopeVariables([
+            'app' => $this->container,
+        ]);
 
         return $shell->run();
     }
 
-    private function getIncludes(): array
+    protected function getCommands()
     {
-        return [
-            __DIR__ . DIRECTORY_SEPARATOR . 'functions.php',
-        ];
+        $names = $this->commandLoader->getNames();
+        $commands = array_map(fn($name) => $this->commandLoader->get($name), $names);
+
+        return $commands;
     }
+
 }
